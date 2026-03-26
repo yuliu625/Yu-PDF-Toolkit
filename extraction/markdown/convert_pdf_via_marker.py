66bb19849c0ac934e6c4857d20b843aec775f23a
yu <yul1024@outlook.com>
2026-03-26 12:01:25 +0800

    refactor: move

---
+++ extraction/markdown/convert_pdf_via_marker.py
@@ -0,0 +1,19 @@
+"""
+Sources:
+    https://github.com/yuliu625/Yu-PDF-Toolkit/extraction/markdown/convert_pdf_via_marker.py
+
+References:
+
+
+Synopsis:
+    通过marker转换pdf为markdown。
+
+Notes:
+
+"""
+
+from __future__ import annotations
+from loguru import logger
+
+from typing import TYPE_CHECKING
+# if TYPE_CHECKING:

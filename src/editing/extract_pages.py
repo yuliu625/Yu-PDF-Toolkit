7b7e4aa5a706dba74c02bc4389e13925e32b7fff
yu <yul1024@outlook.com>
2026-06-19 13:47:56 +0800

    refactor: rename and move

--- src/editing/extract_pages.py
+++ src/editing/extract_pages.py
@@ -1,6 +1,6 @@
 """
 Sources:
-    https://github.com/yuliu625/Yu-PDF-Toolkit/blob/main/modules/editing/extract_pages.py
+    https://github.com/yuliu625/Yu-PDF-Toolkit/blob/main/src/editing/extract_pages.py
 
 References:
     https://pymupdf.readthedocs.io/en/latest/the-basics.html

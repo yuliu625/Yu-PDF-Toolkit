417a2229339045a2d1058f2603daeb404a357f5a
yu <yul1024@outlook.com>
2026-06-19 13:47:44 +0800

    refactor: rename and move

--- src/extraction/markdown/convert_pdf_via_mineru.py
+++ src/extraction/markdown/convert_pdf_via_mineru.py
@@ -1,6 +1,6 @@
 """
 Sources:
-    https://github.com/yuliu625/Yu-PDF-Toolkit/blob/main/modules/extraction/markdown/convert_pdf_via_mineru.py
+    https://github.com/yuliu625/Yu-PDF-Toolkit/blob/main/src/extraction/markdown/convert_pdf_via_mineru.py
 
 References:
 

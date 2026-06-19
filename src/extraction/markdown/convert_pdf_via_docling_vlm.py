fd059b5afbd54c96c057612dfd127143604fab86
yu <yul1024@outlook.com>
2026-06-19 13:47:36 +0800

    refactor: rename and move

--- src/extraction/markdown/convert_pdf_via_docling_vlm.py
+++ src/extraction/markdown/convert_pdf_via_docling_vlm.py
@@ -1,6 +1,6 @@
 """
 Sources:
-    https://github.com/yuliu625/Yu-PDF-Toolkit/blob/main/modules/extraction/markdown/convert_pdf_via_docling_vlm.py
+    https://github.com/yuliu625/Yu-PDF-Toolkit/blob/main/src/extraction/markdown/convert_pdf_via_docling_vlm.py
 
 References:
     https://www.docling.ai/
